d03c71579f031bd20311eed363842de643202007
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-29 13:18:17 +1000

    индивидуальное изменения

--- Window4.xaml.cs
+++ Window4.xaml.cs
@@ -23,5 +23,50 @@ namespace ISRPO_Cherednichenko_PR11
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainCanvas.Children.Clear();
+
+            int N = 10;
+            try
+            {
+                N = Convert.ToInt32(FigureCount.Text);
+            }
+            catch (Exception ee)
+            {
+                this.Title = "Только целое число!";
+            }
+            GenerteShapes(N);
+        }
+
+        private void GenerteShapes(int N)
+        {
+            Random rndButton = new Random(DateTime.Now.Millisecond);
+            Random rndStyle = new Random(DateTime.Now.Millisecond);
+            Random rndPosition = new Random(DateTime.Now.Millisecond);
+            Random rndSize = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < N; i++)
+            {
+                Button currentShape;
+                int shapeType = rndButton.Next(0, 2);
+                currentShape = new Button();
+                
+
+                int shareStyle = rndStyle.Next(0, 4) + 1;
+                String styleName = "style" + shareStyle.ToString();
+                Style currentStyle = (Style)this.FindResource(styleName);
+                currentShape.Style = currentStyle;
+
+                currentShape.Width = rndSize.Next(15, 250);
+                currentShape.Height = rndSize.Next(20, 200);
+
+                MainCanvas.Children.Add(currentShape);
+                Canvas.SetLeft(currentShape, rndPosition.Next(15, 700));
+                Canvas.SetTop(currentShape, rndPosition.Next(15, 300));
+            }
+        }
+
     }
 }
