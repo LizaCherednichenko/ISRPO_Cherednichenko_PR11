0f29e0c3a16d8389d028c2352abc04daeb1a1720
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-29 12:45:47 +1000

    готовая практическая

--- Window3.xaml.cs
+++ Window3.xaml.cs
@@ -37,5 +37,36 @@ namespace ISRPO_Cherednichenko_PR11
             }
             GenerteShapes(N);
         }
+
+        private void GenerteShapes(int N)
+        {
+            Random rndShapeType = new Random(DateTime.Now.Millisecond);
+            Random rndStyle = new Random(DateTime.Now.Second);
+            Random rndPosition = new Random(DateTime.Now.Minute);
+            Random rndSize = new Random(DateTime.Now.Minute);
+
+            for (int i=0; i<N; i++)
+            {
+                Shape currentShape;
+                int shapeType = rndShapeType.Next(0, 2);
+                if (shapeType == 0)
+                    currentShape = new Ellipse();
+                else
+                    currentShape = new Rectangle();
+
+                int shareStyle = rndStyle.Next(0, 3) + 1;
+                String styleName = "style" + shareStyle.ToString();
+                Style currentStyle = (Style)this.FindResource(styleName);
+                currentShape.Style = currentStyle;
+
+                currentShape.Width = rndSize.Next(10, 200);
+                currentShape.Height = rndSize.Next(10, 100);
+
+                MainCanvas.Children.Add(currentShape);
+                Canvas.SetLeft(currentShape, rndPosition.Next(5, 750));
+                Canvas.SetTop(currentShape, rndPosition.Next(5, 350));
+            }
+        }
+
     }
 }
